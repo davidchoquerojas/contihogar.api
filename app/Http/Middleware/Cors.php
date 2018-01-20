debafbd6f9040cf810ce605452ab353236a70a3e
davidchoquerojas <choquerojasnedavid@gmail.com>
2018-01-20 10:04:17 -0500

    cambios 20 01 2018

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,11 +13,15 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        return $next($request)
+    public function handle($request, Closure $next){
+        if ($request->isMethod('OPTIONS')){
+            $response = Response::make();
+        } else {
+            $response = $next($request);
+        }
+        return $response
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
     }
 }
